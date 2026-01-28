ea674d00d93f29d7a3d10d6b40600e4a6445cd81
devanandana21 <tve24el018@cet.ac.in>
2026-01-28 13:24:34 +0530

    Update SpiralMatrix.java

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -1,8 +1,60 @@
+import java.util.*;
+
 public class SpiralMatrix {
-    
-        // TODO: Read r and c
-        // TODO: Read the matrix elements
-        // TODO: Print the elements in spiral order
-        // Hint: Use 4 pointers: top, bottom, left, right
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[][] mat = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        int top = 0, bottom = r - 1;
+        int left = 0, right = c - 1;
+
+        boolean first = true;
+
+        while (top <= bottom && left <= right) {
+
+            // Top row
+            for (int j = left; j <= right; j++) {
+                if (!first) System.out.print(" ");
+                System.out.print(mat[top][j]);
+                first = false;
+            }
+            top++;
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(" " + mat[i][right]);
+            }
+            right--;
+
+            // Bottom row
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    System.out.print(" " + mat[bottom][j]);
+                }
+                bottom--;
+            }
+
+            // Left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(" " + mat[i][left]);
+                }
+                left++;
+            }
+        }
+
+        sc.close();
+    }
 }
